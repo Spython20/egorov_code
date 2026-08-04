813bba4ce01167628b3ce16f45c3eb8553a3e9bb
Spython20 <71393868+Spython20@users.noreply.github.com>
2026-08-04 13:58:48 -0500

    Add configuration parameters in config.py
    Initialize configuration parameters for simulation.

---
+++ config.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy as s
+
+"""good matching parameters, d = 1, epsilon = 0.1, q0 = 2.0, p0 = 1.0, final_time = 10.0"""
+epsilon = 0.1
+d = 1
+d1 = 0.1
+q0 = 2.0
+p0 = 1.0
+N = 100000 # number of samples
+dt = 0.01
+final_time = 5.0
+n_steps = int(round(final_time / dt))
+
+# matrix for initial gaussian wavepacket
+D1 = np.diag([d1])
+D2 = s.linalg.block_diag(D1, np.reciprocal(D1))
+
+# how many H_alpha(z) we want
+basis_size = 5
+
+number_of_trials = 100
+target_time = 5.0          # change to 6.0 when needed
