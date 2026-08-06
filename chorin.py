6bfa53b975c2675a547e45a3d0c027d2e9a53571
Spython20 <71393868+Spython20@users.noreply.github.com>
2026-08-06 07:55:26 -0500

    Remove debug print statements in chorin.py
    Commented out print statements for debugging.

--- chorin.py
+++ chorin.py
@@ -15,7 +15,7 @@ def H(k,q,p):
     i = k
     while i >= 0:
         value = value + (-1.0) ** np.abs(k) * laguerre_coefficient(i, k) * ((1 / epsilon) * (z_origin_squared(q,p)))**i
-        print("computed H coefficient " + str(i))
+        #print("computed H coefficient " + str(i))
         i = i - 1    
     return value
 
@@ -23,7 +23,7 @@ def chorin_step_one(observable_flow_composed, q_samples, p_samples):
     c = []
     for i in range(0, basis_size):
         c.append((1 / N) * np.sum(observable_flow_composed * H(i, q_samples, p_samples),axis = 1))
-        print("computed chorin coefficient " + str(i))
+        #print("computed chorin coefficient " + str(i))
 
     return c
 
@@ -34,7 +34,7 @@ def chorin_step_two(observable_flow_composed, c_alpha, q_samples, p_samples):
     # sum over cn * Hn
     for i in range(1, c_alpha.shape[0]):
         correction += c_alpha[i][:, None] * H(i, q_samples, p_samples)[None, :]
-        print("computed corrected coefficient " + str(i))
+        #print("computed corrected coefficient " + str(i))
 
     corrected_values = observable_flow_composed - correction
 
