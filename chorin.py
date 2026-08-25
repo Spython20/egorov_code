88a8bb74a77bfb2cbcb7388392a7a75dc497e0e5
Spython20 <71393868+Spython20@users.noreply.github.com>
2026-08-25 14:01:30 -0500

    Refactor functions for laguerre polynomials and corrections

--- chorin.py
+++ chorin.py
@@ -3,26 +3,33 @@ from scipy.special import comb, factorial
 from config import *
 
 # phase space centred z
-def z_origin_squared(q, p):
-    return d1 * (q - q0) ** 2 + ((p - p0) ** 2) / d1
+def z_1(q1, p1):
+    return d1 * (q1 - q10) ** 2 + ((p1 - p10) ** 2) / d1
+
+def z_2(q2, p2):
+    return d2 * (q2 - q20) ** 2 + ((p2 - p20) ** 2) / d2
 
 # laguerre polynomial coefficients
 def laguerre_coefficient(j,k):
     return((-1)**j * comb(k,k-j)) / factorial(j)
 
-def H(k,q,p):
+def laguerre(k, x):
     value = 0
     i = k
     while i >= 0:
-        value = value + (-1.0) ** np.abs(k) * laguerre_coefficient(i, k) * ((1 / epsilon) * (z_origin_squared(q,p)))**i
+        value += laguerre_coefficient(i, k) * x**i
         #print("computed H coefficient " + str(i))
-        i = i - 1    
+        i -= 1
     return value
+
+def H(k, q, p):
+    k1, k2 = k
+    return (-1.0) ** (k1 + k2) * laguerre(k1, z_1(q[:, 0], p[:, 0]) / epsilon) * laguerre(k2, z_2(q[:, 1], p[:, 1]) / epsilon)
 
 def chorin_step_one(observable_flow_composed, q_samples, p_samples):
     c = []
-    for i in range(0, basis_size):
-        c.append((1 / N) * np.sum(observable_flow_composed * H(i, q_samples, p_samples),axis = 1))
+    for k in basis_indices:
+        c.append((1 / N) * np.sum(observable_flow_composed * H(k, q_samples, p_samples),axis = 1))
         #print("computed chorin coefficient " + str(i))
 
     return c
@@ -33,7 +40,8 @@ def chorin_step_two(observable_flow_composed, c_alpha, q_samples, p_samples):
 
     # sum over cn * Hn
     for i in range(1, c_alpha.shape[0]):
-        correction += c_alpha[i][:, None] * H(i, q_samples, p_samples)[None, :]
+        k = basis_indices[i]
+        correction += c_alpha[i][:, None] * H(k, q_samples, p_samples)[None, :]
         #print("computed corrected coefficient " + str(i))
 
     corrected_values = observable_flow_composed - correction
